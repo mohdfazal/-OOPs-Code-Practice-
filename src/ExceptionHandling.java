b0fbdf52781e0776245bc9fb20b3878a9f23d58b
mohdfazal <mf4357452@gmail.com>
2024-04-20 06:22:54 +0530

    ExeptionHandlingTheory.txt file added

--- src/ExceptionHandling.java
+++ src/ExceptionHandling.java
@@ -1,5 +1,36 @@
+import java.util.Scanner;
+
 public class ExceptionHandling {
     public static void main(String[] args) {
+        int a;
+        int b;
+
+           Scanner sc = new Scanner(System.in);
+           a = sc.nextInt();
+           b = sc.nextInt();
+
+           try {
+               int ans = a / b;
+               System.out.println(ans);
+
+//               int arr[] = new int[4];
+//               for (int i = 0; i <= 4; i++) {
+//                   arr[i] = sc.nextInt();
+//               }
+           }
+            catch (ArithmeticException e) {
+               System.out.println("This is ArithmeticException  " + e.getMessage());
+           }
+           catch (ArrayIndexOutOfBoundsException e){
+               System.out.println(e.getMessage());
+           }
+//           catch (Exception e) {
+//               System.out.println("this is final exceptio we are getting by " + e.getMessage());
+//           }
+          finally {
+               System.out.println("this block will code anyhow");
+           }
+        System.out.println("post try catch finally block ");
 
     }
 }
